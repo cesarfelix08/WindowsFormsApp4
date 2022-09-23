af15e38a82e5964a2ea6b74d0a64b5d91ba6e25a
cf384 <cf384@M3>
2022-09-23 16:06:27 -0600

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.multiplicacion = new System.Windows.Forms.Button();
+            this.division = new System.Windows.Forms.Button();
+            this.resta = new System.Windows.Forms.Button();
+            this.suma = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // multiplicacion
+            // 
+            this.multiplicacion.Location = new System.Drawing.Point(327, 265);
+            this.multiplicacion.Name = "multiplicacion";
+            this.multiplicacion.Size = new System.Drawing.Size(91, 21);
+            this.multiplicacion.TabIndex = 19;
+            this.multiplicacion.Text = "MULTIPLICAR";
+            this.multiplicacion.UseVisualStyleBackColor = true;
+            // 
+            // division
+            // 
+            this.division.Location = new System.Drawing.Point(350, 222);
+            this.division.Name = "division";
+            this.division.Size = new System.Drawing.Size(68, 21);
+            this.division.TabIndex = 18;
+            this.division.Text = "DIVIDIR";
+            this.division.UseVisualStyleBackColor = true;
+            // 
+            // resta
+            // 
+            this.resta.Location = new System.Drawing.Point(350, 175);
+            this.resta.Name = "resta";
+            this.resta.Size = new System.Drawing.Size(68, 21);
+            this.resta.TabIndex = 17;
+            this.resta.Text = "RESTA";
+            this.resta.UseVisualStyleBackColor = true;
+            // 
+            // suma
+            // 
+            this.suma.Location = new System.Drawing.Point(350, 134);
+            this.suma.Name = "suma";
+            this.suma.Size = new System.Drawing.Size(68, 24);
+            this.suma.TabIndex = 16;
+            this.suma.Text = "SUMA";
+            this.suma.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(160, 245);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(131, 20);
+            this.textBox3.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(160, 168);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(131, 20);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(160, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 20);
+            this.textBox1.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Resultado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Número2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Número1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 421);
+            this.Controls.Add(this.multiplicacion);
+            this.Controls.Add(this.division);
+            this.Controls.Add(this.resta);
+            this.Controls.Add(this.suma);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button multiplicacion;
+        private System.Windows.Forms.Button division;
+        private System.Windows.Forms.Button resta;
+        private System.Windows.Forms.Button suma;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
